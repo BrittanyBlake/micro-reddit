53704cc5ab35bcb713e0fdd65b976437b5c84b39
Brittany Blake <brittany.blake49@gmail.com>
2020-06-02 15:44:35 +0200

    add association between posts and users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    validates :title, presence:true, length:{minimum:5, maximum:25} 
+    validates :body, presence:true, length:{minimum:5, maximum:250} 
+
 end
