e3b43dafbbedf75a23f869848d9dfc1953f1ae17
adam <aabou9@gmail.com>
2020-06-02 12:20:31 +0100

    test

---
+++ vendor/bundle/ruby/2.6.0/gems/ffi-1.13.0/ext/ffi_c/extconf.h
@@ -0,0 +1,9 @@
+#ifndef EXTCONF_H
+#define EXTCONF_H
+#define HAVE_FFI_H 1
+#define HAVE_FFI_CLOSURE_ALLOC 1
+#define HAVE_FFI_PREP_CIF_VAR 1
+#define HAVE_FFI_RAW_CALL 1
+#define HAVE_FFI_PREP_RAW_CLOSURE 1
+#define HAVE_RAW_API 1
+#endif
