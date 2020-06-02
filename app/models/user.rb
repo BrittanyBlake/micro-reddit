d2d54dd9a6f71a8caa1dc643e703df56a4107515
Brittany Blake <brittany.blake49@gmail.com>
2020-06-02 11:18:52 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length:{minimum:3, maximum:25}, uniqueness:{case_sensitive:false}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+     validates :email, presence: true, length:{minimum:3, maximum:25}, uniqueness:{case_sensitive:false}, format:{with:VALID_EMAIL_REGEX}
     has_secure_password
 end
