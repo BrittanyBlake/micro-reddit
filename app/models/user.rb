ee298dc04a36cf4f0d655009433ae6a2e78056c7
Brittany Blake <brittany.blake49@gmail.com>
2020-06-03 11:12:01 +0200

    remove password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { minimum: 3, maximum: 25 },
                     uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_135325) do
+ActiveRecord::Schema.define(version: 2020_06_03_090930) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_06_02_135325) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
